696e2c7573bc8550f38886cfba99d9d75e55c1ab
nguyenminhtiend <nguyenminhtiend@gmail.com>
2017-02-04 01:36:41 +0700

    #add google map

--- public/app/components/dashboard/jobOnMap/jobOnMap.js
+++ public/app/components/dashboard/jobOnMap/jobOnMap.js
@@ -3,9 +3,7 @@
 
     angular.module('main.module')
         .component('jobOnMap', {
-            bindings: {
-
-            },
+            bindings: {},
             templateUrl: 'app/components/dashboard/jobOnMap/jobOnMap.html',
             controller: JobOnMapController,
             controllerAs: 'vm'
@@ -15,45 +13,30 @@
     JobOnMapController.$inject = ['NgMap'];
     function JobOnMapController(NgMap) {
         var vm = this;
-
-        vm.googleMapsUrl="https://maps.googleapis.com/maps/api/js?key=AIzaSyACK-A7YHo0jyMzEQQHarZw8uMikeQ379k";
-
-        NgMap.getMap().then(function(map) {
-            vm.showCustomMarker= function(evt) {
-                map.customMarkers.foo.setVisible(true);
-                map.customMarkers.foo.setPosition(this.getPosition());
-            };
-            vm.closeCustomMarker= function(evt) {
-                this.style.display = 'none';
-            };
-        });
-
-        vm.map = { center: { latitude: 1.290270, longitude: 103.851959 }, zoom: 13 };
+        vm.showDetail = showDetail;
 
         vm.$onInit = function () {
-            vm.markers = [
+            NgMap.getMap().then(function (map) {
+                vm.map = map;
+            });
+
+            vm.jobs = [
                 {
-                    id: 1,latitude: 1.290270, longitude: 103.851959, title: 'Maker 1'
+                    id: '123',
+                    address: '20 Ubi Avenue 4, Singapore',
+                    title: 'Marker 1'
                 },
                 {
-                    id: 2, latitude: 1.390270, longitude: 103.8519359, title: 'Maker 2'
+                    id: '633',
+                    address: 'Ethoz Group Ltd., Tampines Street 92, Singapore',
+                    title: 'Marker 2'
                 }
             ];
-
-            // var geocoder = new google.maps.Geocoder();
-            // geocoder.geocode( { "address": "20 Ubi Avenue 4, Singapore" }, function(results, status) {
-            //     if (status == google.maps.GeocoderStatus.OK && results.length > 0) {
-            //         var location = results[0].geometry.location,
-            //             lat      = location.lat(),
-            //             lng      = location.lng();
-            //         vm.map = { center: { latitude: lat, longitude: lng }, zoom: 8 };
-            //     }
-            // });
-        }
-
-        vm.onClick = function(marker, eventName, model) {
-            vm.currentMarker = marker;
-            model.show = !model.show;
         };
+
+        function showDetail(e, job) {
+            vm.currentJob = job;
+            vm.map.showInfoWindow('job-info', job.id);
+        }
     }
 })();
